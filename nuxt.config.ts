c0a58eda598ec36d04bbfb3b5330caff8b6a7bf8
Thomas Cameron <68596478+thomas-k-cameron@users.noreply.github.com>
2023-07-07 23:39:14 +0000

    asdf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'], // 修正
+  css: ['vuetify/lib/styles/main.sass'], // 修正
   build: {
-    transpile: ['vuetify', /vue3-library-reproduction/],
+    transpile: ['vuetify', 'mdi/css/materialdesignicons.min.css'],
   },
   vite: {
     define: {
